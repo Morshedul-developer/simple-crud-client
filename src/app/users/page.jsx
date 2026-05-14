679a7c5b9fece13b7df468b3027bebf08081b05a
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-05-14 08:32:08 +0600

    added a modal based contact form

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,3 +1,4 @@
+import AddUserModal from "../components/AddUserModal";
 import UsersTable from "../components/UsersTable";
 import { deleteUser } from "../lib/actions";
 import { getUsers } from "../lib/data";
@@ -6,7 +7,10 @@ const UsersPage = async () => {
     const users = await getUsers();
     return (
         <div className="px-50 pt-5 space-y-5">
-            <h1 className="text-4xl font-bold">User Management</h1>
+            <div className="flex justify-between">
+                <h1 className="text-4xl font-bold">User Management</h1>
+                <AddUserModal></AddUserModal>
+            </div>
             <p className="text-xl font-semibold">Total users : {users.length}</p>
             <UsersTable users={users} deleteUserAction={deleteUser}></UsersTable>
         </div>
